27c79836523146d38a564bd360dc247a8f9e03a0
= <=>
2021-07-07 13:52:03 -0300

    Finalizando exercício

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            try
+            {
+                Console.WriteLine("-- Exercício Tabuada --");
+                Console.Write("Recepção de uma valor e exibição da tabuada desse ");
+                Console.WriteLine("valor até o limite de 10 números com laços de repetição.");
+                Console.WriteLine();
+
+                Console.Write("Insira o valor a ser calculado: ");
+                int numeroTabuada = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                for (int cont = 1; cont < 11; cont++)
+                {
+                    int resultado = numeroTabuada * cont;
+
+                    Console.WriteLine($"--> {numeroTabuada}x{cont} = {resultado}");
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Atenção!! O valor de entrada não está no formato correto.");
+                Console.ResetColor();
+            }
+
+            catch (OverflowException)
+            {
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Atenção!! O valor de entrada excede o limite.");
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# tabuada
-Recepção de uma valor e exibição da tabuada desse valor até o limite estipulado.
+# _tabuada_
+Recepção de uma valor e exibição da tabuada desse valor até o limite de 10 números
+com laços de repetição.
